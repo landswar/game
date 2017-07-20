5e904377ee0aaa40ffbaff95969a0c5443e3ec2b
Ludovic Lerus <lerus_l@epitech.eu>
2017-07-20 23:43:19 +0200

    game handle tilemap

--- public/src/states/game/StateGame.js
+++ public/src/states/game/StateGame.js
@@ -27,37 +27,33 @@ class StateGame extends Phaser.State {
 	 * Preload sprites and plugins.
 	 */
 	preload() {
-		this.load.image('groundImage', groundImg);
+		this.load.image('tiles', groundImg);
 		this.load.image('cursor', cursorImg);
 		this.load.image('circleNbTurn', circleNbTurnImg);
 		this.game.kineticScrolling = this.game.plugins.add(Phaser.Plugin.KineticScrolling);
+
+		const blob = new Blob([JSON.stringify(landsWarData.getMap())], {type: "application/json"})
+		this.load.tilemap('ground', URL.createObjectURL(blob), null, Phaser.Tilemap.TILED_JSON);
 	}
 
 	/**
 	 * Display the map.
 	 */
 	_displayMap() {
-		this._tileMap = this.add.tilemap();
-		this._tileMap.addTilesetImage('groundImage');
+		this._tileMap = this.add.tilemap('ground');
+		this._tileMap.addTilesetImage('standard-sun', 'tiles');
 
-		this._map = landsWarData.getMap();
-		const groundLayer = this._tileMap.create('groundLayer', this._map[0].length, this._map.length, TILE_SIZE, TILE_SIZE, this._mapGroup);
-		groundLayer.resizeWorld();
-
-		for (let y = 0; y < this._map.length; ++y) {
-			for (let x = 0; x < this._map[y].length; ++x) {
-				this._tileMap.putTile(this._map[y][x], x, y, groundLayer);
-			}
-		}
+		this._groundLayer = this._tileMap.createLayer('groundLayer');
+		this._groundLayer.resizeWorld();
 	}
 
 	/**
 	 * Diplay the map as a background and add other elements in top of it.
 	 */
 	create() {
-		this._mapGroup = this.add.group();
-
 		this._displayMap();
+
+		this._mapGroup = this.add.group();
 		this._cursor = this.add.sprite(0, 0, 'cursor');
 		this._mapGroup.add(this._cursor);
 
@@ -74,6 +70,8 @@ class StateGame extends Phaser.State {
 			horizontalWheel:  false,
 			verticalWheel:    false,
 		});
+
+		// To get Tile data : this._tileMap.getTile(0, 0, 'groundLayer');
 	}
 
 	// update() { }
